02f23c4d55e4557c4d2104e8a01e340d5aaaf9ae
Juan Corso <juanadriancorso94@gmail.com>
2022-02-11 17:35:13 -0300

    logre crear los botones dinamicos de las dos tarjetas Skills

--- js/events.js
+++ js/events.js
@@ -11,6 +11,43 @@ function copiado() {
 
 }
 
-function mostrarSkills() {
+//=============================================================================//
+
+window.onload = verHard();
+
+function verSoft() {
+
+    const elementoSoftSkills = document.querySelector("#card-soft");
+    const elementoHardSkills = document.querySelector("#card-hard");
+    elementoHardSkills.style.visibility = "hidden";
+    elementoSoftSkills.style.visibility = "visible";
+
+    const textoHardSkill = document.querySelector(".hard");
+    textoHardSkill.style.backgroundColor = 'var(--colorNegro)';
+    textoHardSkill.style.color = 'var(--colorLetraBlanca)';
+
+    const textoSoftSkill = document.querySelector(".soft");
+    textoSoftSkill.style.backgroundColor = 'var(--colorGris)';
+    textoSoftSkill.style.color = 'var(--colorNegro)';
+
+
+}
+
+function verHard() {
+    const elementoHardSkills = document.querySelector("#card-hard");
+    const elementoSoftSkills = document.querySelector("#card-soft");
+    elementoSoftSkills.style.visibility = "hidden";
+    elementoHardSkills.style.visibility = "visible";
+
+    const textoHardSkill = document.querySelector(".hard");
+    textoHardSkill.style.backgroundColor = 'var(--colorGris)';
+    textoHardSkill.style.color = 'var(--colorNegro)';
+
+    const textoSoftSkill = document.querySelector(".soft");
+    textoSoftSkill.style.backgroundColor = 'var(--colorNegro)';
+    textoSoftSkill.style.color = 'var(--colorLetraBlanca)';
+
+
+
 
 }
